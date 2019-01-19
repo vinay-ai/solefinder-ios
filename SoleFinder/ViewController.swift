265983f4269f7e3263f2524d405ff02c31c3fb1e
vinay-ai <vinaykolwankar@gmail.com>
2019-01-19 12:19:55 +0530

    Created xcode project

---
+++ SoleFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoleFinder
+//
+//  Created by Vinay Kolwankar on 19/01/19.
+//  Copyright © 2019 Vinay Kolwankar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
