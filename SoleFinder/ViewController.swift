8da155bad41d0b709a0e4c3ce7ef61457104ed40
vinay-ai <vinaykolwankar@gmail.com>
2019-01-19 17:36:23 +0530

    Gave user the functionality to import photos from the camera or the photo library

--- SoleFinder/ViewController.swift
+++ SoleFinder/ViewController.swift
@@ -8,37 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-
-    @IBOutlet weak var categoryLabel: UILabel!
     let model = SoleIdentifier()
-    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
     }
+    
+
+    @IBAction func ChooseImage(_ sender: Any) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action: UIAlertAction) in
+            imagePickerController.sourceType = .camera
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }
+            else
+            {
+                print("Camera not available")
+            }
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+         actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action: UIAlertAction) in
+             imagePickerController.sourceType = .photoLibrary
+             self.present(imagePickerController, animated: true, completion: nil)
+         }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        imageView.image = image
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         let imageView = sender.view as? UIImageView
         
-        if let imageToAnalyse = imageView?.image {
-            if let SoleLabelString = SoleLabel(forImage: imageToAnalyse) {
-                categoryLabel.text = SoleLabelString
+        if let imagetoAnalyse = imageView?.image {
+            if let soleLabelString = soleLabel(forImage: imagetoAnalyse){
+                categoryLabel.text = soleLabelString
             }
-        }
-    }
-    
-    func SoleLabel (forImage image:UIImage) -> String? {
-        if let pixelBuffer = ImageProcessor.pixelBuffer(forImage: image.cgImage!) {
-            guard let sole = try? model.prediction(image: pixelBuffer) else {fatalError("Unexpected runtime error")}
-            return sole.classLabel
             
         }
-        
-        return nil
     }
     
-
+    func soleLabel (forImage image:UIImage)-> String? {
+        if let pixelBuffer = ImageProcessor.pixelBuffer(forImage: image.cgImage!){
+            guard let sole = try? model.prediction(image: pixelBuffer) else {
+                fatalError("Unexpected runtime error")
+            }
+            return sole.classLabel
+        }
+        return nil
+    }
 
 }
 
