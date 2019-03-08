74dbe9c3a2f7abecacee4498b2b8cc2d8ec7ee06
vinay-ai <vinaykolwankar@gmail.com>
2019-03-08 18:08:05 +0530

    Handeled CustomAlert exceptions

--- SoleFinder/ViewController.swift
+++ SoleFinder/ViewController.swift
@@ -11,8 +11,28 @@ import SafariServices
 
 public var SoleIndex = ""
 public var SoleIndexString = ""
+var mainVC = ViewController()
+
+extension UIApplication {
+    class func topViewController(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let navigationController = controller as? UINavigationController {
+            return topViewController(controller: navigationController.visibleViewController)
+        }
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return topViewController(controller: selected)
+            }
+        }
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        return controller
+    }
+}
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, SFSafariViewControllerDelegate {
+    
+    
     
     let model = SoleIdentifier()
     let reachability =  Reachability()!
@@ -134,6 +154,29 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
             
         }
+    }
+    
+    
+    @objc func onClickAmazon() {
+        if let topController = UIApplication.topViewController() {
+            let SafariVCA = SFSafariViewController(url: NSURL(string: "https://www.amazon.in/s?i=shoes&field-keywords=\(SoleIndexString)")! as URL)
+            topController.present(SafariVCA, animated: true, completion: nil)
+            
+        }
+    }
+    
+    
+    @objc func onClickFlipkart() {
+        if let topController = UIApplication.topViewController() {
+            let SafariVCF = SFSafariViewController(url: NSURL(string: "https://www.flipkart.com/search?q=\(SoleIndexString)")! as URL)
+            topController.present(SafariVCF, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        controller.dismiss(animated: true, completion: nil)
     }
     
     
